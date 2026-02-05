b7d9ad7cf32a5d5fc2450608044c04d1124a3445
Linuskramshoej <linus.kramshoej@protonmail.com>
2026-02-05 11:07:38 +0100

    ,da

--- src/src/TivoliRide.java
+++ src/src/TivoliRide.java
@@ -2,21 +2,22 @@
 public class TivoliRide {
 
     private int height;
+    private int rideHeight;
 
-    public TivoliRide(int height) {
+    public TivoliRide(int height, int rideHeight) {
         this.height = height;
-
+        this.rideHeight = rideHeight;
     }
 
     public String getTivoliRide() {
 
-        if ((height > 185)) {
+        if ((height > rideHeight)) {
             return "Enjoy the Ride.";
 
-        } else if ((height < 185)) {
+        } else if ((height < rideHeight)) {
             return "Bare ærgeligt brormand";
 
-        } else if ((height == 185)){
+        } else if ((height == rideHeight)){
             return "Der var du heldig, det var lige før du ikke kom med. ";
         }
 
--- src/src/TivoliRideTest.java
+++ src/src/TivoliRideTest.java
@@ -7,9 +7,13 @@ public class TivoliRideTest {
         System.out.println("Indtast din højde");
 
         int height = tivoliScanner.nextInt();
-        TivoliRide tivoliRide1 = new TivoliRide(height);
+        TivoliRide tivoliRide1 = new TivoliRide(height,185);
+        TivoliRide tivoliRide2 = new TivoliRide(height,160);
+        TivoliRide tivoliRide3 = new TivoliRide(height,200);
 
-        System.out.println(tivoliRide1.getTivoliRide());
+        System.out.println("Ride 1: "+ tivoliRide1.getTivoliRide());
+        System.out.println("Ride 2: "+ tivoliRide2.getTivoliRide());
+        System.out.println("Ride 3: "+ tivoliRide3.getTivoliRide());
 
         tivoliScanner.close();
     }
