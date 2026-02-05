d71d2b90e68c669dc0eb40f83e207003faa6cb81
FrankHamp <valdemarengelund@gmail.com>
2026-02-05 10:51:03 +0100

    jaaaa

--- src/src/TivoliRide.java
+++ src/src/TivoliRide.java
@@ -1,5 +1,25 @@
+
 public class TivoliRide {
-    public static void main (String[] args){
-        System.out.println("Hello Valdemar");
+
+    private int height;
+
+    public TivoliRide(int height) {
+        this.height = height;
+
+    }
+
+    public String getTivoliRide() {
+
+        if ((height > 185)) {
+            return "Enjoy the Ride.";
+
+        } else if ((height <= 185)) {
+            return "Bare ærgeligt brormand";
+
+        } else if ((height == 185)){
+            return "Der var du heldig, det var lige før du ikke kom med. ";
+        }
+
+        return "Hjælp mig jeg har brug for en Lambo";
     }
 }
